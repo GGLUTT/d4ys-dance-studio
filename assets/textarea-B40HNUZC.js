be25b887919a4a95b5568ecf0cb9f45f92bda318
GGLUTT <161070189+GGLUTT@users.noreply.github.com>
2026-02-01 22:26:16 +0000

    Deploying to gh-pages from @ GGLUTT/d4ys-dance-studio@4a06cde4e506f8583bcc2f68d538d61d8b06aac9 🚀

---
+++ assets/textarea-B40HNUZC.js
@@ -0,0 +1,11 @@
+import{c as e,r as a,j as t,a as n}from"./index-xYMJgWvd.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=e("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=e("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]),u=null,i=a.forwardRef(({className:o,...r},s)=>t.jsx("textarea",{className:n("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",o),ref:s,...r}));i.displayName="Textarea";export{d as C,i as T,l as a,u as s};
